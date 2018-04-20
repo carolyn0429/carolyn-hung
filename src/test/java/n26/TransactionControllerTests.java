6ab3eb8ea8da514bfec7e739883d993dce56516c
Carolyn Hung <carolynhung@Carolyns-MacBook-Pro-2.local>
2018-04-19 21:04:49 -0700

    add unit test

--- src/test/java/n26/TransactionControllerTests.java
+++ src/test/java/n26/TransactionControllerTests.java
@@ -1,5 +1,7 @@
 package n26;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -9,13 +11,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.mockito.Mockito.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -30,6 +35,8 @@ public class TransactionControllerTests {
 
     @Test
     public void paramTransactionIdShouldReturnTransaction() throws Exception {
+
+        // Arrange
         Transaction txn1 = new Transaction(1l, "cars", 20.00);
 
         when(serviceMock.getTransactionById(1l)).thenReturn(txn1);
@@ -44,6 +51,35 @@ public class TransactionControllerTests {
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(jsonPath("$.sum").value("50.0"));
+    }
+
+    @Test
+    public void addNewTransactionWithIdShouldReturnTransactionSuccessfully() throws Exception {
+
+        // Arrange
+        Transaction newTransaction = new Transaction(100, "rent", 1500.00, 2l);
+        ObjectMapper mapper =  new ObjectMapper();
+        String request = mapper.writeValueAsString(newTransaction);
+
+        this.mockMvc.perform(put("/transactionservice/transaction/{id}", 100)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(request))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+    }
+
+    @Test
+    public void paramTypeShouldReturnTransactions() throws Exception {
+
+        List<Long> expectedValue = new ArrayList<>();
+        expectedValue.add(1l);
+        expectedValue.add(2l);
+        MvcResult result =mockMvc.perform(get("/transactionservice/types/{type}", "groceries"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andReturn();
+        Assert.assertEquals("[1,2]", result.toString());
+
     }
 
 }
